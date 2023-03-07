c06e9045396aa8996ba5edb9acbd553f0f69ed16
Lucasuarezmoreno <126017453+Lucasuarezmoreno@users.noreply.github.com>
2023-03-06 21:44:23 -0300

    Commit

---
+++ effects2.js
@@ -0,0 +1,41 @@
+const typed = new Typed('.line', {
+    strings: [''],
+    typeSpeed:75,
+    starDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor:true,
+    cursorChar:'_',
+});
+
+const typed2 = new Typed('.yo', {
+    strings: ['Developer.'],
+    typeSpeed:75,
+    starDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor:true,
+    cursorChar:'_',
+});
+
+const typed3 = new Typed('.conocerme', {
+    strings: ['I am a young man passionate about programming, cooking and coffee. Discipline, responsibility and commitment are three factors that define me. I like to always give my best in everything, no matter how simple or complex the task may be.'],
+    typeSpeed:20,
+    starDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:false,
+    loopCount:false,
+    showCursor:true,
+    cursorChar:'_',
+});
